9133ebca85d0eba1eae574a7e1e80681e23d50c3
thinkingape46 <jagadeesh.kumar2091@gmail.com>
2020-05-24 17:40:23 +0530

    HTML with JavaScript files
    for learning purposes only.

---
+++ JavaScript/javascript_a.js
@@ -0,0 +1,6 @@
+alert("Welcome! Enjoy your stay.");
+
+var deposit = prompt("How much do you like to deposit?  ")
+
+alert("You have depoisted: " +deposit)
+console.log("You are a cool person!")
---
+++ JavaScript/pound_kilo.js
@@ -0,0 +1,6 @@
+var lbVal = prompt("Please enter the weight in Pounds: ");
+var kgVal = lbVal * 0.453592;
+
+alert(`The weight in Kilograms is: ${kgVal}`)
+
+console.log("Conversion to Kilograms completed")
