36a619a6899359339ff02d62bd2754d0ec579725
thinkingape46 <jagadeesh.kumar2091@gmail.com>
2020-05-26 11:33:01 +0530

    some js files related to cotrol flow, while and for loops added.

---
+++ JavaScript/control_flow.js
@@ -0,0 +1,11 @@
+for (var dis = 0; dis <= 10; dis = dis + 2) {
+
+    if (dis == 5) {
+        console.log(`You have run ${dis} kms!`)
+    }
+    else if (dis == 10) {
+        console.log(`You have run ${dis} kms!`)
+    }
+
+}
+console.log("\nYou have finished 10K!")
---
+++ JavaScript/loops_exercises.js
@@ -0,0 +1,49 @@
+// Problem 1
+// Use a For Loop to print (console.log()) out the word "hello" 5 times.
+//
+// Do this with a While Loop and a For Loop
+
+// Using while loop:
+
+// var i = 1
+// while (i < 6) {
+//     console.log("hello")
+//     i += 1
+// }
+
+// Using For loop:
+
+// for (i = 1; i < 6; i += 1) {
+//     console.log("hello")
+// }
+
+
+// -----------------------------------------------------------------------------------------------
+
+/////////////////
+// PROBLEM 2 ///
+///////////////
+
+// Use Loops to console.log() (print out) all the odd numbers from 1 to 25
+// Do this using two methods, a while loop and a for loop
+
+// METHOD ONE
+// While Loop
+
+// x = 1
+// while (x < 26) {
+//     if (x % 2 != 0) {
+//         console.log(`${x} is an odd number`)        
+//     }
+// x += 1
+// }
+
+
+// METHOD TWO
+// For Loop
+
+// for (num = 1; num < 26; num += 1) {
+//     if (num % 2 != 0) {
+//         console.log(`${num} is an odd number`)
+//     }
+// }
