93b14a9e28c05c162b61c473d48bf868b16d4199
thinkingape46 <jagadeesh.kumar2091@gmail.com>
2020-05-31 21:53:32 +0530

    Update to connect four code

--- jQuery/connect_four.js
+++ jQuery/connect_four.js
@@ -1,7 +1,7 @@
 // Display the header.
 // Display the connect-4 board
 // Ask the player whether they like to play?
-    // Some button at the bottom reacts to the question.
+//     Some button at the bottom reacts to the question.
 // Ask the name of players
 // A function to randomly choose: who's going to select the color of the chip first
 // Ask the toss winner to choose the color of chip.
@@ -36,7 +36,7 @@
 //     AskChipChoice()    
 //     console.log(`${playerOne} colour is ${playerColor[0]}, ${playerTwo} color is ${playerColor[1]}`)
 
-//     playGame()
+//     playOneTurnFunc ()
 
 // }
 
@@ -71,11 +71,261 @@
 
 //     if (playerColor[0] === "r") {
 //         playerColor[1] = "y"
+//         chipColors = ["red", "yellow"]
 //     }
-//     else { playerColor[1] = "r"}
+//     else { 
+//         playerColor[1] = "r";
+//         chipColors = ["yellow", "red"]   
+//     }
 
-//     return playerColor
+//     return chipColors
 // }
 
-// function playGame() {
 
+clicks = [1, 1]
+$("td").click(function() {
+    clicks.push(1);
+    console.log(clicks)
+})
+
+if ((clicks.length) % 2 == 0) {
+    color = "green"
+}
+else {
+    color = "red"
+}
+
+var i = 5
+var j = 5
+var k = 5
+var l = 5
+var m = 5
+var n = 5
+var o = 5
+
+$(".colOne").eq(0).click(function() {
+
+    if ((clicks.length) % 2 == 0) {
+        color = "green"
+    }
+    else {
+        color = "red"
+    }    
+    
+    $(".colOne").eq(i).css("background-color", color);
+    console.log(i)
+
+    i = i - 1
+    console.log(i)
+    
+    }
+)
+
+$(".colTwo").eq(0).click(function() {
+    
+    if ((clicks.length) % 2 == 0) {
+        color = "green"
+    }
+    else {
+        color = "red"
+    }
+
+    $(".colTwo").eq(j).css("background-color", color);
+    console.log(i)
+
+    j = j - 1
+    console.log(i)
+    
+    }
+)
+
+$(".colThree").eq(0).click(function() {
+    
+    if ((clicks.length) % 2 == 0) {
+        color = "green"
+    }
+    else {
+        color = "red"
+    }
+
+    $(".colThree").eq(k).css("background-color", color);
+    console.log(i)
+
+    k = k - 1
+    console.log(i)
+    
+    }
+)
+
+$(".colFour").eq(0).click(function() {
+    
+    if ((clicks.length) % 2 == 0) {
+        color = "green"
+    }
+    else {
+        color = "red"
+    }
+
+    $(".colFour").eq(l).css("background-color", color);
+    console.log(i)
+
+    l = l - 1
+    console.log(i)
+    
+    }
+)
+
+
+$(".colFive").eq(0).click(function() {
+    
+    if ((clicks.length) % 2 == 0) {
+        color = "green"
+    }
+    else {
+        color = "red"
+    }
+
+    $(".colFive").eq(m).css("background-color", color);
+    console.log(i)
+
+    m = m - 1
+    console.log(i)
+    
+    }
+)
+
+$(".colSix").eq(0).click(function() {
+    
+    if ((clicks.length) % 2 == 0) {
+        color = "green"
+    }
+    else {
+        color = "red"
+    }
+
+    $(".colSix").eq(n).css("background-color", color);
+    console.log(i)
+
+    n = n - 1
+    console.log(i)
+    
+    }
+)
+
+$(".colSeven").eq(0).click(function() {
+    
+    if ((clicks.length) % 2 == 0) {
+        color = "green"
+    }
+    else {
+        color = "red"
+    }
+
+    $(".colSeven").eq(o).css("background-color", color);
+    console.log(i)
+
+    o = o - 1
+    console.log(i)
+    
+    }
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function playOneTurnFunc() {
+//     console.log(`${playerOne} drop you disc`)
+
+//     $("td").eq(0).click(function () {
+//     if ($("td").eq(35).css("background-color") === "rgb(255, 255, 255)") {
+//         $("td").eq(35).css("background-color", chipColors[0])
+//     }
+//     else if ($("td").eq(28).css("background-color") === "rgb(255, 255, 255)") {
+//         $("td").eq(28).css("background-color", "rgb(153, 0, 0)")
+//     }
+//     else if ($("td").eq(21).css("background-color") === "rgb(255, 255, 255)") {
+//         $("td").eq(21).css("background-color", "rgb(153, 0, 0)")
+//     }
+//     else if ($("td").eq(14).css("background-color") === "rgb(255, 255, 255)") {
+//         $("td").eq(14).css("background-color", "rgb(153, 0, 0)")
+//     }
+//     else if ($("td").eq(7).css("background-color") === "rgb(255, 255, 255)") {
+//         $("td").eq(7).css("background-color", "rgb(153, 0, 0)")
+//     }
+//     else if ($("td").eq(0).css("background-color") === "rgb(255, 255, 255)") {
+//         $("td").eq(0).css("background-color", "rgb(153, 0, 0)")
+//     }
+
+//     playTwoTurnFunc()
+// })
+// }
+
+// function playTwoTurnFunc() {
+//     console.log(`${playerTwo} drop you disc`)
+
+//     $("td").eq(0).click(function () {
+//     if ($("td").eq(35).css("background-color") === "rgb(255, 255, 255)") {
+//         $("td").eq(35).css("background-color", chipColors[1])
+//     }
+//     else if ($("td").eq(28).css("background-color") === "rgb(255, 255, 255)") {
+//         $("td").eq(28).css("background-color", "rgb(242, 255, 0)")
+//     }
+//     else if ($("td").eq(21).css("background-color") === "rgb(255, 255, 255)") {
+//         $("td").eq(21).css("background-color", "rgb(242, 255, 0)")
+//     }
+//     else if ($("td").eq(14).css("background-color") === "rgb(255, 255, 255)") {
+//         $("td").eq(14).css("background-color", "rgb(242, 255, 0)")
+//     }
+//     else if ($("td").eq(7).css("background-color") === "rgb(255, 255, 255)") {
+//         $("td").eq(7).css("background-color", "rgb(242, 255, 0)")
+//     }
+//     else if ($("td").eq(0).css("background-color") === "rgb(255, 255, 255)") {
+//         $("td").eq(0).css("background-color", "rgb(242, 255, 0)")
+//     }
+
+//     playTwoTurnFunc()
+// })
+// }
+
+// // $("td").eq(0).click(function () {
+// //     if ($("td").eq(35).css("background-color") === "rgb(255, 255, 255)") {
+// //         $("td").eq(35).css("background-color", "rgb(153, 0, 0)")
+// //     }
+// //     else if ($("td").eq(28).css("background-color") === "rgb(255, 255, 255)") {
+// //         $("td").eq(28).css("background-color", "rgb(153, 0, 0)")
+// //     }
+// //     else if ($("td").eq(21).css("background-color") === "rgb(255, 255, 255)") {
+// //         $("td").eq(21).css("background-color", "rgb(153, 0, 0)")
+// //     }
+// //     else if ($("td").eq(14).css("background-color") === "rgb(255, 255, 255)") {
+// //         $("td").eq(14).css("background-color", "rgb(153, 0, 0)")
+// //     }
+// //     else if ($("td").eq(7).css("background-color") === "rgb(255, 255, 255)") {
+// //         $("td").eq(7).css("background-color", "rgb(153, 0, 0)")
+// //     }
+// //     else if ($("td").eq(0).css("background-color") === "rgb(255, 255, 255)") {
+// //         $("td").eq(0).css("background-color", "rgb(153, 0, 0)")
+// //     }
+// // })
