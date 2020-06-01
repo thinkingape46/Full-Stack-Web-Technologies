27cdaf198c5ee43db22d91242b7d7a7165fec44c
thinkingape46 <jagadeesh.kumar2091@gmail.com>
2020-06-01 20:09:12 +0530

    connect four game (worked out win conditions)

--- jQuery/connect_four.js
+++ jQuery/connect_four.js
@@ -16,84 +16,91 @@
 // Declare the winner and ask if they want to restart.  (Update the text on the page.)
 
 
-// var playGameQ = prompt("Do you like to play the game (y or n)? ")
-// console.log(playGameQ)
+
+var playGameQ = prompt("Do you like to play the game (y or n)? ")
+console.log(playGameQ)
 
 
-// // Button reaction to playGameQ
-// if (playGameQ == "y") {
-//     $("#buttonOne").css("background-color", "#00b339")
-//     $("#buttonOne").text("Yes")
-//     var playerNames = askName()
-//     console.log(playerNames)
+// Button reaction to playGameQ
+if (playGameQ == "y") {
+    $("#buttonOne").css("background-color", "#00b339")
+    $("#buttonOne").text("Yes")
+    var playerNames = askName()
+    console.log(playerNames)
 
-//     var tossResult = toss()
-//     console.log(`toss result: ${tossResult}`)
-//     var playerOne = playerNames[tossResult[0]]
-//     var playerTwo = playerNames[tossResult[1]]       
-//     console.log(`Toss Winner: ${playerOne}, Toss Loser: ${playerTwo}`)
+    var tossResult = toss()
+    console.log(`toss result: ${tossResult}`)
+    var playerOne = playerNames[tossResult[0]]
+    var playerTwo = playerNames[tossResult[1]]       
+    console.log(`Toss Winner: ${playerOne}, Toss Loser: ${playerTwo}`)
 
-//     AskChipChoice()    
-//     console.log(`${playerOne} colour is ${playerColor[0]}, ${playerTwo} color is ${playerColor[1]}`)
+    AskChipChoice()    
+    console.log(`${playerOne} colour is ${chipColors[0]}, ${playerTwo} color is ${chipColors[1]}`)
 
-//     playOneTurnFunc ()
+    playOneTurnFunc()
 
-// }
+}
 
-// if (playGameQ == "n") {
-//     $("#buttonOne").css("background-color", "#ff0000")
-//     $("#buttonOne").text("No")
-// }
+if (playGameQ == "n") {
+    $("#buttonOne").css("background-color", "#ff0000")
+    $("#buttonOne").text("No")
+}
 
 // // Toss to decide who's going to select the color of the disk first
 
-// function toss() {
-//     var TossWin = Math.floor(Math.random() * 2)
-//     if (TossWin === 1) {
-//         otherSideOfCoin = 0;
-//     }
-//     else {
-//         otherSideOfCoin = 1;
-//     }
-//     return [TossWin, otherSideOfCoin]
-// }
+function toss() {
+    var TossWin = Math.floor(Math.random() * 2)
+    if (TossWin === 1) {
+        otherSideOfCoin = 0;
+    }
+    else {
+        otherSideOfCoin = 1;
+    }
+    return [TossWin, otherSideOfCoin]
+}
 
-// function askName() {
-//     var pOneName = prompt("Player1, please enter your name: ");
-//     var pTwoName = prompt("Player2, please enter your name: ");
-//     return [pOneName, pTwoName]
-// }
+function askName() {
+    var pOneName = prompt("Player1, please enter your name: ");
+    var pTwoName = prompt("Player2, please enter your name: ");
+    return [pOneName, pTwoName]
+}
 
-// function AskChipChoice() {
+function AskChipChoice() {
 
-//     playerColor = ["", ""]
-//     playerColor[0] = prompt(`${playerOne}, choose your disc color (r or y): `)
+    var playerColor = prompt(`${playerOne}, choose your disc color (r or y): `)
 
-//     if (playerColor[0] === "r") {
-//         playerColor[1] = "y"
-//         chipColors = ["red", "yellow"]
-//     }
-//     else { 
-//         playerColor[1] = "r";
-//         chipColors = ["yellow", "red"]   
-//     }
+    if (playerColor === "r") {
+        chipColors = ["red", "yellow"]
+    }
+    else {
+        chipColors = ["yellow", "red"]   
+    }
 
-//     return chipColors
-// }
+    return chipColors
+}
 
+function playOneTurnFunc() {
 
-clicks = [1, 1]
+    $(".updates").text(`${playerOne}, you turn to drop the chip`)
+
+    $(".rowOne").eq(0).click(playTwoTurnFunc())
+}
+
+function playTwoTurnFunc() {
+
+    $(".updates").text(`${playerTwo}, you turn to drop the chip`)
+
+    $(".rowOne").eq(0).click(playOneTurnFunc())
+
+}
+
+var clicks = [1, 1]
 $("tr").eq(0).click(function() {
     clicks.push(1);
     console.log(clicks)
 })
 
-if ((clicks.length) % 2 == 0) {
-    color = "green"
-}
-else {
-    color = "red"
-}
+// FILL CELLS START
 
 var i = 5
 var j = 5
@@ -107,17 +114,16 @@ $(".colOne").eq(0).click(function() {
 
         if (i >= 0) {
             if ((clicks.length) % 2 == 0) {
-                color = "green"
+                color = chipColors[0]
             }
             else {
-                color = "red"
+                color = chipColors[1]
             }    
             
             $(".colOne").eq(i).css("background-color", color);
-            console.log(i)
+            winCheck()
         
             i = i - 1
-            console.log(i)
         }
         else {
             alert(columnFull())
@@ -129,17 +135,16 @@ $(".colTwo").eq(0).click(function() {
 
     if (j >= 0) {
         if ((clicks.length) % 2 == 0) {
-            color = "green"
+            color = chipColors[0]
         }
         else {
-            color = "red"
+            color = chipColors[1]
         }
     
         $(".colTwo").eq(j).css("background-color", color);
-        console.log(i)
+        winCheck()
     
         j = j - 1
-        console.log(i)
     }
     else {
         alert(columnFull())
@@ -151,17 +156,16 @@ $(".colThree").eq(0).click(function() {
 
     if (k >= 0) {
         if ((clicks.length) % 2 == 0) {
-            color = "green"
+            color = chipColors[0]
         }
         else {
-            color = "red"
+            color = chipColors[1]
         }
     
         $(".colThree").eq(k).css("background-color", color);
-        console.log(i)
+        winCheck()
     
         k = k - 1
-        console.log(i)
     }
     else {
         alert(columnFull())
@@ -173,17 +177,16 @@ $(".colFour").eq(0).click(function() {
 
     if (l >= 0) {
         if ((clicks.length) % 2 == 0) {
-            color = "green"
+            color = chipColors[0]
         }
         else {
-            color = "red"
+            color = chipColors[1]
         }
     
         $(".colFour").eq(l).css("background-color", color);
-        console.log(i)
+        winCheck()
     
         l = l - 1
-        console.log(i)
     }
     else {
         alert(columnFull())
@@ -196,17 +199,16 @@ $(".colFive").eq(0).click(function() {
 
     if (m >= 0) {
         if ((clicks.length) % 2 == 0) {
-            color = "green"
+            color = chipColors[0]
         }
         else {
-            color = "red"
+            color = chipColors[1]
         }
     
         $(".colFive").eq(m).css("background-color", color);
-        console.log(i)
+        winCheck()
     
         m = m - 1
-        console.log(i)
     }
     else {
         alert(columnFull())
@@ -218,17 +220,16 @@ $(".colSix").eq(0).click(function() {
 
     if (n >= 0) {
         if ((clicks.length) % 2 == 0) {
-            color = "green"
+            color = chipColors[0]
         }
         else {
-            color = "red"
+            color = chipColors[1]
         }
     
         $(".colSix").eq(n).css("background-color", color);
-        console.log(i)
+        winCheck()
     
         n = n - 1
-        console.log(i)
     }
     else {
         alert(columnFull())
@@ -240,17 +241,16 @@ $(".colSeven").eq(0).click(function() {
 
     if (o >= 0) {
         if ((clicks.length) % 2 == 0) {
-            color = "green"
+            color = chipColors[0]
         }
         else {
-            color = "red"
+            color = chipColors[1]
         }
     
         $(".colSeven").eq(o).css("background-color", color);
-        console.log(i)
+        winCheck()
     
         o = o - 1
-        console.log(i)
     }
     else {
         alert(columnFull())
@@ -263,131 +263,127 @@ function columnFull() {
     return "column is full, please choose another"    
 }
 
+// FILL CELLS END
+
 cellValue = ""
 function winCheck() {
-    cellValue = ""
-    var value = ""
-    for (q = 0; q < 6; q++) {
-        if ($(".colOne").eq(q).css("background-color") === "rgb(0, 128, 0)") {
-            value = "g"
-        }
-        else if ($(".colOne").eq(q).css("background-color") === "rgb(255, 0, 0)") {
-            value = "r"
-        }
-        cellValue = cellValue + value
-    }
-    // for (strPos = 0; strPos < cellValue.length + 1; strPos++) {
-    //     if(cellValue[strPos] === cellValue[strPos + 1] && cellValue[strPos] === cellValue[strPos + 2] && cellValue[strPos] === cellValue[strPos + 3] && cellValue[strPos] != undefined) {
-    //         return "Someone won!"
-    //     }
-    //     else {console.log("No wins!")}        
-    // }
-    for (pos = 0; pos < 42; pos++) {
-        if ($("td").eq(pos).css("background-color") === $("td").eq(pos + 1).css("background-color") && 
-        $("td").eq(pos).css("background-color") === $("td").eq(pos + 2).css("background-color") && 
-        $("td").eq(pos).css("background-color") === $("td").eq(pos + 3).css("background-color") &&
-        $("td").eq(pos).css("background-color") != "rgb(255, 255, 255)")
-            return "Someone Won!"
-        else {
-            console.log("No Wins!")
+
+// WIN CHECK IN ROWS
+
+    for (td = 0; td < 36; td = td + 7) {
+
+        for (row = td; row < td + 4; row++){
+            if($("td").eq(row).css("background-color") === $("td").eq(row + 1).css("background-color") &&
+            $("td").eq(row).css("background-color") === $("td").eq(row + 2).css("background-color") &&
+            $("td").eq(row).css("background-color") === $("td").eq(row + 3).css("background-color") &&
+            $("td").eq(row).css("background-color") != "rgb(255, 255, 255)")
+            { 
+                $("td").eq(row).css("border-color", "rgb(0, 255, 0)")
+                $("td").eq(row + 1).css("border-color", "rgb(0, 255, 0)")
+                $("td").eq(row + 2).css("border-color", "rgb(0, 255, 0)")
+                $("td").eq(row + 3).css("border-color", "rgb(0, 255, 0)")
+                return "win"
+            }
         }
     }
 
-console.log(cellValue)  
+// WIN CHECK IN COLUMNS
+
+    for (td = 0; td < 7; td++) {
+
+        for (col = td; col < td + 36; col = col + 7) {
+            if($("td").eq(col).css("background-color") === $("td").eq(col + 7).css("background-color") &&
+            $("td").eq(col).css("background-color") === $("td").eq(col + 14).css("background-color") &&
+            $("td").eq(col).css("background-color") === $("td").eq(col + 21).css("background-color") &&
+            $("td").eq(col).css("background-color") != "rgb(255, 255, 255)")
+            {
+                $("td").eq(col).css("border-color", "rgb(0, 255, 0)")
+                $("td").eq(col + 7).css("border-color", "rgb(0, 255, 0)")
+                $("td").eq(col + 14).css("border-color", "rgb(0, 255, 0)")
+                $("td").eq(col + 21).css("border-color", "rgb(0, 255, 0)")
+                return "win"
+            }            
+        }
+    }
+
+//  WIN CHECK IN CROSS DIRECTION - TOP RIGHT TO BOTTOM LEFT
+
+    for (td = 3; td < 7; td++) {
+        var limCheck = {3: 3, 4: 10, 5: 17, 6: 18}
+
+        for (cr = td; cr <= limCheck[td]; cr = cr + 6) {
+            
+            if($("td").eq(cr).css("background-color") === $("td").eq(cr + 6).css("background-color") &&
+            $("td").eq(cr).css("background-color") === $("td").eq(cr + 12).css("background-color") &&
+            $("td").eq(cr).css("background-color") === $("td").eq(cr + 18).css("background-color") &&
+            $("td").eq(cr).css("background-color") != "rgb(255, 255, 255)")
+            {
+                $("td").eq(cr).css("border-color", "rgb(0, 255, 0)")
+                $("td").eq(cr + 6).css("border-color", "rgb(0, 255, 0)")
+                $("td").eq(cr + 12).css("border-color", "rgb(0, 255, 0)")
+                $("td").eq(cr + 18).css("border-color", "rgb(0, 255, 0)")
+                return "win"
+            }
+        }
+    }
+    
+    for (td = 13; td < 21; td = td + 7) {
+        var limCheck = {13: 19, 20: 20}
+
+        for (cr = td; cr <= limCheck[td]; cr = cr + 6) {
+            
+            if($("td").eq(cr).css("background-color") === $("td").eq(cr + 6).css("background-color") &&
+            $("td").eq(cr).css("background-color") === $("td").eq(cr + 12).css("background-color") &&
+            $("td").eq(cr).css("background-color") === $("td").eq(cr + 18).css("background-color") &&
+            $("td").eq(cr).css("background-color") != "rgb(255, 255, 255)")
+            {
+                $("td").eq(cr).css("border-color", "rgb(0, 255, 0)")
+                $("td").eq(cr + 6).css("border-color", "rgb(0, 255, 0)")
+                $("td").eq(cr + 12).css("border-color", "rgb(0, 255, 0)")
+                $("td").eq(cr + 18).css("border-color", "rgb(0, 255, 0)")
+                return "win"
+            }
+        }
+    }
+
+//  WIN CHECK IN CROSS DIRECTION - TOP LEFT TO BOTTOM RIGHT
+
+for (td = 3; td > -1; td--) {
+    var limCheck = {3: 3, 2: 10, 1: 17, 0: 16}
+
+    for (cr = td; cr <= limCheck[td]; cr = cr + 8) {
+        
+        if($("td").eq(cr).css("background-color") === $("td").eq(cr + 8).css("background-color") &&
+        $("td").eq(cr).css("background-color") === $("td").eq(cr + 16).css("background-color") &&
+        $("td").eq(cr).css("background-color") === $("td").eq(cr + 24).css("background-color") &&
+        $("td").eq(cr).css("background-color") != "rgb(255, 255, 255)")
+        {
+            $("td").eq(cr).css("border-color", "rgb(0, 255, 0)")
+            $("td").eq(cr + 8).css("border-color", "rgb(0, 255, 0)")
+            $("td").eq(cr + 16).css("border-color", "rgb(0, 255, 0)")
+            $("td").eq(cr + 24).css("border-color", "rgb(0, 255, 0)")
+            return "win"
+        }
+    }
 }
 
+for (td = 7; td < 15; td = td + 7) {
+    var limCheck = {7: 15, 14: 14}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function playOneTurnFunc() {
-//     console.log(`${playerOne} drop you disc`)
-
-//     $("td").eq(0).click(function () {
-//     if ($("td").eq(35).css("background-color") === "rgb(255, 255, 255)") {
-//         $("td").eq(35).css("background-color", chipColors[0])
-//     }
-//     else if ($("td").eq(28).css("background-color") === "rgb(255, 255, 255)") {
-//         $("td").eq(28).css("background-color", "rgb(153, 0, 0)")
-//     }
-//     else if ($("td").eq(21).css("background-color") === "rgb(255, 255, 255)") {
-//         $("td").eq(21).css("background-color", "rgb(153, 0, 0)")
-//     }
-//     else if ($("td").eq(14).css("background-color") === "rgb(255, 255, 255)") {
-//         $("td").eq(14).css("background-color", "rgb(153, 0, 0)")
-//     }
-//     else if ($("td").eq(7).css("background-color") === "rgb(255, 255, 255)") {
-//         $("td").eq(7).css("background-color", "rgb(153, 0, 0)")
-//     }
-//     else if ($("td").eq(0).css("background-color") === "rgb(255, 255, 255)") {
-//         $("td").eq(0).css("background-color", "rgb(153, 0, 0)")
-//     }
-
-//     playTwoTurnFunc()
-// })
-// }
-
-// function playTwoTurnFunc() {
-//     console.log(`${playerTwo} drop you disc`)
-
-//     $("td").eq(0).click(function () {
-//     if ($("td").eq(35).css("background-color") === "rgb(255, 255, 255)") {
-//         $("td").eq(35).css("background-color", chipColors[1])
-//     }
-//     else if ($("td").eq(28).css("background-color") === "rgb(255, 255, 255)") {
-//         $("td").eq(28).css("background-color", "rgb(242, 255, 0)")
-//     }
-//     else if ($("td").eq(21).css("background-color") === "rgb(255, 255, 255)") {
-//         $("td").eq(21).css("background-color", "rgb(242, 255, 0)")
-//     }
-//     else if ($("td").eq(14).css("background-color") === "rgb(255, 255, 255)") {
-//         $("td").eq(14).css("background-color", "rgb(242, 255, 0)")
-//     }
-//     else if ($("td").eq(7).css("background-color") === "rgb(255, 255, 255)") {
-//         $("td").eq(7).css("background-color", "rgb(242, 255, 0)")
-//     }
-//     else if ($("td").eq(0).css("background-color") === "rgb(255, 255, 255)") {
-//         $("td").eq(0).css("background-color", "rgb(242, 255, 0)")
-//     }
-
-//     playTwoTurnFunc()
-// })
-// }
-
-// // $("td").eq(0).click(function () {
-// //     if ($("td").eq(35).css("background-color") === "rgb(255, 255, 255)") {
-// //         $("td").eq(35).css("background-color", "rgb(153, 0, 0)")
-// //     }
-// //     else if ($("td").eq(28).css("background-color") === "rgb(255, 255, 255)") {
-// //         $("td").eq(28).css("background-color", "rgb(153, 0, 0)")
-// //     }
-// //     else if ($("td").eq(21).css("background-color") === "rgb(255, 255, 255)") {
-// //         $("td").eq(21).css("background-color", "rgb(153, 0, 0)")
-// //     }
-// //     else if ($("td").eq(14).css("background-color") === "rgb(255, 255, 255)") {
-// //         $("td").eq(14).css("background-color", "rgb(153, 0, 0)")
-// //     }
-// //     else if ($("td").eq(7).css("background-color") === "rgb(255, 255, 255)") {
-// //         $("td").eq(7).css("background-color", "rgb(153, 0, 0)")
-// //     }
-// //     else if ($("td").eq(0).css("background-color") === "rgb(255, 255, 255)") {
-// //         $("td").eq(0).css("background-color", "rgb(153, 0, 0)")
-//     }
-// })
+    for (cr = td; cr <= limCheck[td]; cr = cr + 8) {
+        
+        if($("td").eq(cr).css("background-color") === $("td").eq(cr + 8).css("background-color") &&
+        $("td").eq(cr).css("background-color") === $("td").eq(cr + 16).css("background-color") &&
+        $("td").eq(cr).css("background-color") === $("td").eq(cr + 24).css("background-color") &&
+        $("td").eq(cr).css("background-color") != "rgb(255, 255, 255)")
+        {
+            $("td").eq(cr).css("border-color", "rgb(0, 255, 0)")
+            $("td").eq(cr + 8).css("border-color", "rgb(0, 255, 0)")
+            $("td").eq(cr + 16).css("border-color", "rgb(0, 255, 0)")
+            $("td").eq(cr + 24).css("border-color", "rgb(0, 255, 0)")
+            return "win"
+        }
+    }
+}
+ 
+}
