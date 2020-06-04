06a1318acae47ffdaaf1b8420e867c9a47832676
thinkingape46 <jagadeesh.kumar2091@gmail.com>
2020-06-04 12:45:51 +0530

    added a new method for generating random numbers. also some changes to function match_check, close_checkc and guess_game

--- Python/number_guess_game.py
+++ Python/number_guess_game.py
@@ -1,20 +1,33 @@
 # Number guss game.
 
+# Generate a random number
+
 from random import randint
+from random import shuffle
 
 def random_three_num ():
     '''
     Generates a random three digit number with no repeating number.
     '''
     random_number = randint(102, 987)
-
     random_num = str(random_number)
 
     if random_num[0] == random_num[1] or random_num[0] == random_num[2] or random_num[1] == random_num[2]:
         random_three_num()
-
     else:
         return random_num
+
+# Another way of generating three digti number.
+
+def random_gen():
+
+    list = [str(num) for num in range(0, 10)]
+    shuffle(list)
+
+    random_num = list[0] + list[1] + list[2]
+    print(random_num)
+
+# Ask the player whether they like to play.
 
 def like_to_play():
 
@@ -22,53 +35,63 @@ def like_to_play():
 
     if choice not in ["yes", "no"]:
         like_to_play()
-
     elif choice == "yes":
         guess_game()
-
     elif choice == "no":
         print("See you later!")
 
+# A function to check if there is a match.
+
 def match_check(a, b):
-    
+    '''
+    A function to check if there is a match. Takes two number in the form of a string, (player_guess_number, random number).
+    '''
+    clue = []
     for num in [0, 1, 2]:
         if a[num] == b[num]:
-            return "Match"
+            clue.append("Match")
+    return " ".join(clue)
+
+# A function to check whether any digit in the guess matches with random number
 
 def close_check(a, b):
-
+    '''  A function to check whether any digit in the guess matches with the one in random number. Takes two number in the form of a string, (player_guess_number, random number).  '''
+    clue = []
     for char in a:
         if char in b:
-            return "Close"
-    return "Nope"
+            clue.append("Close")
+    return " ".join(clue)
 
 def guess_game():
+    ''' A function that keeps asking the player for the guess until the correct number is guessed. Returns if the guess number is a Match, CLose or No match.   '''
 
     random_number = str(random_three_num())
 
-    while True:
+    game_state = []
+
+    while len(game_state) == 0:
 
         try:
             player_guess = input("Please enter your guess: ")
 
         except:
-            print("I am not using this")
+            print("Enter three digits please..")
 
         if player_guess == random_number:
             print("Great! it's the right guess")
-            break
+            game_state.append("win")
 
-        elif close_check(player_guess, random_number) == "Nope":
-            print("Nope")
+        elif match_check(player_guess, random_number) != "":
+            print(match_check(player_guess, random_number))
             continue
 
-        elif match_check(player_guess, random_number) == "Match":
-            print("It's a Match")
-            continue   
+        elif close_check(player_guess, random_number) == "":
+            print("Nope! Sorry")
+            continue
 
-        elif close_check(player_guess, random_number) == "Close":
-            print("It's Close")
-            continue  
+        elif close_check(player_guess, random_number) != []:
+            print(close_check(player_guess, random_number))
+            continue
 
     like_to_play()
 
