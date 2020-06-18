cb9140a11122fc85e5923c9cc6c97cd9817cbf60
thinkingape46 <jagadeesh.kumar2091@gmail.com>
2020-06-18 21:40:18 +0530

    learning flask

--- Flask/adoption_site/adoption_site.py
+++ Flask/adoption_site/adoption_site.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from forms import AddForm, DelForm
 
 # create a flask app
 app = Flask(__name__)
@@ -22,7 +23,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # Create a databse by passing application into SQLAlchemy
 db = SQLAlchemy(app)
 # Giving the ability to migrate.
-Migrate(db, app)
+Migrate(app, db)
 
 # Creat a model puppy:
 
@@ -39,7 +40,7 @@ class Puppy(db.Model):
         self.name = name
 
     def __repr__(self):
-        return "Puppy's name is {a}".format(a=self.name)
+        return "Puppy name is {a}".format(a=self.name)
 
 # View functions.
 
@@ -48,3 +49,47 @@ def index():
     return render_template('home.html')
 
 @app.route('/add_puppy', methods=['GET', 'POST'])
+def add_puppy():
+
+    form = AddForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+
+        new_puppy = Puppy(name)
+        db.session.add(new_puppy)
+        db.session.commit()
+        return redirect(url_for('list_puppy'))
+
+    return render_template('add.html', form=form)
+
+@app.route('/del_puppy', methods=['GET', 'POST'])
+def del_puppy():
+
+    form = DelForm()
+
+    if form.validate_on_submit():
+
+        id = form.id.data
+        # Get the name of the puppy.
+        puppy = Puppy.query.get(id)
+
+        db.session.delete(puppy)
+        db.session.commit()
+        
+        return redirect(url_for('list_puppy'))
+
+    return render_template('delete.html', form=form)
+
+@app.route('/list_puppies')
+def list_puppy():
+
+    puppies = Puppy.query.all()
+
+    return render_template('list.html', puppies=puppies)
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ Flask/adoption_site/forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SubmitField
+
+class AddForm(FlaskForm):
+
+    name = StringField('Name of the Puppy:')
+    submit = SubmitField('Add Puppy')
+
+class DelForm(FlaskForm):
+
+    id = IntegerField("Id number of Puppy to remove:")
+    submit = SubmitField('Remove Puppy')
