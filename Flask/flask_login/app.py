7ff78bdf1ef48bba0564c0a27b7ad0af9e0a18f0
thinkingape46 <jagadeesh.kumar2091@gmail.com>
2020-06-22 15:46:31 +0530

    learning flask

---
+++ Flask/flask_login/app.py
@@ -0,0 +1,73 @@
+# app.py
+
+from myproject import app, db
+from flask import render_template, url_for, redirect, request, flash, abort
+from flask_login import login_user, login_required, logout_user
+from myproject.models import User
+from myproject.forms import LoginForm, RegistrationForm
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/welcome')
+# It makes sure that for the user to see the welcome view, they have to login first.
+@login_required
+def welcome_user():
+    return render_template('wecome_user.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash(message='You have Logged out!')
+    return redirect(url_for('home'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        # Get the user by the email provided.
+        # first() is used, because emails will be unique.
+        user = User.query.filter_by(email=form.email.data).first()
+
+        if user.check_password(password=form.password.data) and user is not None:
+            login_user(user)
+            flash(message='Logged In Succesfully')
+
+            # Next is used when the login view is triggered because tried to visit a page that requires a login.
+            next = request.args.get('next')
+
+            if next == None or not next[0] == '/':
+                next = url_for('welcome_user.html')
+
+            return redirect(next)
+
+    return render_template('login.html', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+
+        email = form.email.data
+        username = form.username.data
+        password = form.password.data
+
+        user = User(email, username, password)
+
+        db.session.add(user)
+        db.session.commit()
+        flash('Thanks for registeration')
+
+        return redirect(url_for('login'))
+
+    return render_template('register.html', form=form)
+
+
---
+++ Flask/flask_login/myproject/forms.py
@@ -0,0 +1,29 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, ValidationError
+from wtforms.validators import DataRequired, Email, EqualTo
+from models import User
+
+class LoginForm(FlaskForm):
+
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired()])
+    log_in = SubmitField(label='Log In')
+
+class RegistrationForm(FlaskForm):
+    
+    # DataRequired() makes sure that field is filled.
+    # EqualTo() checks if the certain fields are given the same input.
+    # 'label' is what shown to the user.
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    username = StringField(label='Username', validators=[DataRequired()])
+    password = PasswordField(label='Password', validators=[DataRequired(), EqualTo('pass_confirm', message='Password must match!')])
+    pass_confirm = PasswordField(label='Confirm Password', validators=[DataRequired()])
+    register = SubmitField(label='Register')
+
+    def check_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email has been already registered')
+
+    def check_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username has been already registered')
