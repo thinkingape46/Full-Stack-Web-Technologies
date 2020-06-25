0c9c3f39dbf451d95d3c8730ad53edda4cd72cf9
thinkingape46 <jagadeesh.kumar2091@gmail.com>
2020-06-25 14:27:51 +0530

    social company blog

---
+++ Flask/social_company_blog/puppy_company_blog/models.py
@@ -0,0 +1,60 @@
+from puppy_company_blog import db, login_manager
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from datetime import datetime
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+class User(db.Model, UserMixin):
+    
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    
+    # We shall be using a link as string.
+    # nullable=False, because every user will hava a default profile image.
+    profile_image = db.Column(db.String(64), nullable=False, default='default_profile.png')
+    
+    # What does it mean by 'index=True'
+    email = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    # passwords are hashed to 128 bit string.
+    password_hash = db.Column(db.String(128))
+    post = db.relationship('BlogSpot', backref='author', lazy=True)
+
+    def __init__(self, email, username, password):
+        self.email = email
+        self.username = username
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return "Username: {a}".format(a=self.username)
+
+
+class BlogPost(db.Model):
+    
+    # setting up the relationship, each blogpost is connected to a user.
+    users = db.relationship(User)
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(users.id), nullable=False)
+    # DateTime storage.
+    date = db.Column(db.DateTime,  nullable=False, default=datetime.utcnow)
+    title = db.Column(db.String(200), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+
+    def __init__(self, title, text, user_id):
+        self.title = title
+        self.text = text
+        self.user_id = user_id
+
+    def __repr__(self):
+        return "Post Id: {a}, Date: {b}".format(a=self.id, b=self.date)
+
+        
---
+++ Flask/social_company_blog/puppy_company_blog/users/forms.py
@@ -0,0 +1,47 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField, ValidationError
+from wtforms.validators import DataRequired, Email, EqualTo
+# This import will allow users to update their profile picture. 
+from flask_wtf.file import FileField, FileAllowed
+
+from flask_login import current_user
+from puppy_company_blog.models import User
+
+class LoginForm(FlaskForm):
+
+    email = StringField('Email: ', validators=[DataRequired(), Email()])
+    password = PasswordField('Password: ', validators=[DataRequired()])
+    submit = SubmitField('Log In')
+
+class RegistrationForm(FlaskForm):
+
+    email = StringField('Email: ', validators=[DataRequired(), Email()])
+    username = StringField('Username: ', validators=[DataRequired()])
+    password = StringField('Password: ', validators=[DataRequired(), EqualTo('pass_confirm', message='Passwords must match')])
+    pass_confirm = StringField('Confirm Password: ', validators=[DataRequired])
+    
+    def check_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('This Email has been already registered!')
+
+    def check_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError("This Username has been already taken")
+
+    submit = SubmitField('Register')
+
+class UpdateUserForm(FlaskForm):
+
+    email = StringField("Email: ", validators=[DataRequired(), Email()])
+    username = StringField("Username: ", validators=[DataRequired()])
+    picture = FileField("Update profile picture", validators=[FileAllowed(['jpg','png'])])
+    submit = SubmitField('Update')
+
+    def check_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('This Email has been already registered.')
+
+    def check_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError("This Username has been already taken")
+        
