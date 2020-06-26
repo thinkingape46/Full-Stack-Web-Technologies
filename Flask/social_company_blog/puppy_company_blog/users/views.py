32a592675caaef7e4bc4a9bb050267eb19755979
thinkingape46 <jagadeesh.kumar2091@gmail.com>
2020-06-26 14:35:19 +0530

    learning flask

--- Flask/social_company_blog/puppy_company_blog/users/views.py
+++ Flask/social_company_blog/puppy_company_blog/users/views.py
@@ -74,9 +74,15 @@ def account():
 
 
 @users.route("/<username>")
-def user_posts():
-        
+def user_posts(username):
 
+    # setting a variable 'page'
+    # helps us to cycle through blog posts using pages.
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    blog_posts = BlogPost.query.filter_by(author=user).order_by(BlogPost.date.desc()).paginate(page=page, per_page=5)
+    return render_template('user_blog_posts.html', blog_posts=blog_posts, user=user)
+    
 
 @users.route('/logout')
 def logout():
