439dfb9d698d8ace009fa6d073bd00a7104da0d0
thinkingape46 <jagadeesh.kumar2091@gmail.com>
2020-07-05 21:39:11 +0530

    work on flask apps

--- Flask/to_do/app.py
+++ Flask/to_do/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, redirect, jsonify, request
 from bmi.forms import BmiCalculator
+from python_scripts.bmi_calculator import bmi_calculator
 
 app = Flask(__name__)
 
@@ -13,22 +14,35 @@ def index():
 def info():
     return render_template('info.html')
 
-@app.route("/bmi")
-def bmi():
-
-    w = request.args.get("w", 1, type=int)
-    h = request.args.get("h", 1, type=int)
-    bmi = w / (h/100 * h/100) 
-
-    return jsonify(result = bmi)
-
-
 @app.route('/bmi_page', methods=['GET', 'POST'])
 def bmi_page():
 
     form = BmiCalculator()
 
     return render_template('bmi.html', form=form)
+
+@app.route("/bmi")
+def bmi():
+
+    w = request.args.get("w", 1, type=int)
+    h = request.args.get("h", 1, type=int)
+
+    bmi = bmi_calculator(w, h)
+
+    return jsonify(result = bmi)
+
+@app.route("/calculator_page")
+def calculator_page():
+    return render_template('calculator.html')
+
+@app.route("/calculator")
+def calculator():
+
+    a = request.args.get("a", "1", type=str)
+    print("result: {a}".format(a = a))
+    return jsonify(result = list(a))
+
+
 
 @app.route('/<name>')
 def user_page(name):
---
+++ Flask/to_do/python_scripts/bmi_calculator.py
@@ -0,0 +1,2 @@
+def bmi_calculator(weight, height):
+    return weight/(height/100 * height/100)
